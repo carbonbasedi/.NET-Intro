54efaf15ec6ba76335ff55a64e762a72489df12c
carbonbasedi <carbonbasedai@gmail.com>
2023-05-24 14:28:34 +0400

    init

---
+++ PP/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PP.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index(){
+			ViewBag.Name = "Ali";
+			ViewBag.Surname = "Aliyev";
+			ViewBag.Age = 25;
+			ViewData["Country"] = "Azerbaijan";
+			ViewData["Number"] = "+994551234567";
+			return View();
+		}
+	}
+}
